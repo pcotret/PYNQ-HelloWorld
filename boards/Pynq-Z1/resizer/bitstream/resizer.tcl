31e05fa2b42d812b2c686ecf281988eec14989a2
Yaman Umuroglu <maltanar@gmail.com>
2020-04-29 01:10:55 +0100

    set Fclk on accel interfaces, validate design

--- boards/Pynq-Z1/resizer/bitstream/resizer.tcl
+++ boards/Pynq-Z1/resizer/bitstream/resizer.tcl
@@ -308,7 +308,10 @@ proc create_root_design { parentCell } {
    CONFIG.PCW_USE_S_AXI_HP0 {1} \
  ] $processing_system7_0
 
+  # set generated Fclk and match on accelerator interfaces
   set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ $::config_ip_fclk] $processing_system7_0
+  set_property CONFIG.FREQ_HZ [expr $::config_ip_fclk*1000000] [get_bd_intf_pins resize_accel_0/$::config_ip_axis_name_in]
+  set_property CONFIG.FREQ_HZ [expr $::config_ip_fclk*1000000] [get_bd_intf_pins resize_accel_0/$::config_ip_axis_name_out]
 
   # Create instance: resize_accel_0, and set properties
   set resize_accel_0 [ create_bd_cell -type ip -vlnv $::config_ip_vlnv resize_accel_0 ]
@@ -338,13 +341,14 @@ proc create_root_design { parentCell } {
 
 
   if { $::config_ip_use_axilite == 1 } {
+    set_property CONFIG.FREQ_HZ [expr $::config_ip_fclk*1000000] [get_bd_intf_pins resize_accel_0/$::config_ip_axilite_name]
     connect_bd_intf_net -intf_net axi_interconnect_0_M01_AXI [get_bd_intf_pins axi_interconnect_0/M01_AXI] [get_bd_intf_pins resize_accel_0/$::config_ip_axilite_name]
     create_bd_addr_seg -range 0x00010000 -offset 0x43C00000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs resize_accel_0/$::config_ip_axilite_name/Reg] SEG_resize_accel_0_Reg
   }
 
   # Restore current instance
   current_bd_instance $oldCurInst
-
+  validate_bd_design
   save_bd_design
 }
 # End of create_root_design()
